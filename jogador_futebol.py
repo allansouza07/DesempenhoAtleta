73749cdbbf4503679653a2367c5224bf8df81961
Allan <allangomes_07@outlook.com>
2026-04-02 01:44:14 -0300

    Desenvolvimento na coleta de dados
    adicionei um sistema que organiza todos os dados coletados, depois farei pra todos os nomes que o usuário digitar

--- jogador_futebol.py
+++ jogador_futebol.py
@@ -15,13 +15,53 @@ jogador["nome do atleta"]= str(input("qual o nome do jogador? "))
 partidas = int(input(f"quantos jogos o {jogador['nome do atleta']} fez? "))
 while i < partidas :
     i += 1
-    gols.append(int(input(f"quantos gols ele fez na {i}° partida")))
+    gols.append(int(input(f"quantos gols ele fez na {i}° partida: ")))
 
 jogador["total"]= total = sum(gols)
 jogador["g/p"]= gp = total / partidas
 jogadores.append(jogador.copy())
 
+#DADOS DO JOGADOR
+print()
+print(f"Confira os dados do {jogador['nome do atleta']}:")
+for c,k in jogador.items():
+    if c == "nome do atleta" :
+        print(f"Nome: {k}")
+    if c == "gols":
+        print(f"Partidas: {partidas}")
+        print(f"Gols: {total}")
+    if c == "g/p":
+        print(f"G/P: {k:.2f} gols a cada partida")
+print()
+
 #CONDIÇÃO (QUER CONTINUAR?)
+i = 0
+a = 0
+while True:
+    i += 1
+    escolha = str (input("quer continuar? "))
+    if escolha in " n N NAO nao ":
+        break
+    elif escolha in " s S SIM sim ":
+        a = 0
+        total = sum(gols)
+        gols = []
+        gp = 0
+        jogador["nome do atleta"] = str(input(f"qual o nome do {i + 1}° atleta: "))
+        partidas = int (input(f"quantas partidas o {jogador['nome do atleta']} jogou? "))
+        while a < partidas:
+            a+= 1
+            gols.append(int(input(f"quantos gols ele fez na {a} partida? ")))
+    else :
+        print("erro, tente novamente!")
+
+    jogador["total"]= sum(gols)
+    jogador["g/p"] = gp = total / partidas
+    jogadores.append(jogador.copy())
+    
+
+
+        
 
 
 print(jogador)
